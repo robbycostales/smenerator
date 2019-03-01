c467a3136cc6176fd4416d9bc8ef6e15a5fef93a
Robby Costales <rcostales15@simons-rock.edu>
2019-03-01 15:52:31 -0500

    finished for now

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ var SMENSTART = ["<span class='first-letter'>S</span>",
                 "<span class='first-letter'>N</span>"]
 
 function smenerate(){
+
   DONE = [0, 0, 0, 0]
   var i;
   for (i=0; i<LETTERS.length; i++){
@@ -19,9 +20,6 @@ function smenerate(){
       url: url,
       type: 'GET',
       crossDomain: true,
-      // dataType: 'jsonp',
-      // jsonpCallback: 'handleRequests',
-      // dataType: 'html',
       success: handleRequests,
       error: function(e, f,g) {
                               console.log(e);
@@ -31,24 +29,63 @@ function smenerate(){
     });
     // $.get(url, handleRequests);
   }
+
 }
 
 function handleRequests(response){
-  console.log(response)
   var i; // iterator index
   var pos; // part of speech
+  var letter;
 
   // get first letter
-  var letter = response[0].word[0]
+  try {
+    letter = response[0].word[0]
+  } catch(error){
+    console.log("no first letter of first word...")
+    console.log(error)
+    return
+  }
+
+  console.log(String(letter) + " has " + String(response.length) + " responses ")
 
   pos = document.getElementById(letter+'-pos').value
-  var randObj = response[Math.floor(Math.random() * response.length)]
-  var tries = 1;
-  while (tries < 1000 && pos != 'NA' && randObj.tags && !randObj.tags.includes(pos)){
-    randObj = response[Math.floor(Math.random() * response.length)]
-    tries++;
+  // true or false, if we want to keep this value, return if so
+  var keep=$("#"+letter+'-keep').is(":checked");
+  if (keep){
+    return
   }
+
+  // fetch random object from response vector
+  randObj = response[Math.floor(Math.random() * response.length)]
+
+  // see if we need to try a new word
+  var tries = 0;
+  var randObj;
+  // if we've tried more times than length of list
+  // ... or, we don't care about tags at all, we peace out
+  console.log(letter+'  leading into while loop')
+  while (tries < 1000 && pos != 'NA'){
+    tries +=1;
+    console.log(tries)
+
+    // if tags undefined
+    if (!randObj.tags){
+      continue
+    }
+
+    // see if object has POS tag we are looking for
+    if (randObj.tags.includes(pos)){
+      break
+    }
+
+    // get new value
+    randObj = response[Math.floor(Math.random() * response.length)]
+    // increment tries, you dummy!
+  }
+
+  // set HTML of corresponding letter to new word generated
   document.getElementById(letter).innerHTML = SMENSTART[LETTERS.indexOf(letter)] + randObj.word.substring(1);
+  console.log(letter+ "  set")
 }
 
 // handle clicking of enter button
